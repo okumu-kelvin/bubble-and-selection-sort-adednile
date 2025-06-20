d89d4b169f830033f09a756d5c9eaa5227b3a68a
PotterIle <elindeda1@gmail.com>
2025-06-20 13:58:02 +0300

    first

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,16 @@
+# TODO: Implement bubble sort
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
+    n = len(unsorted_list)
+    sorted_arr = unsorted_list[:]  # Create a copy of the list
+
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if sorted_arr[j] > sorted_arr[j + 1]:
+                sorted_arr[j], sorted_arr[j + 1] = sorted_arr[j + 1], sorted_arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+    return sorted_arr
     pass
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,17 @@
 def selection_sort(arr):
+    n = len(arr)
     # TODO: Implement selection sort
+    sorted_arr = arr[:]
+
+    for i in range(n):
+        min_index = i
+        
+        for j in range(i + 1, n):
+            if sorted_arr[j] < sorted_arr[min_index]:
+                min_index = j
+    
+        if min_index != i:
+            sorted_arr[i], sorted_arr[min_index] = sorted_arr[min_index], sorted_arr[i]
+
+    return sorted_arr
     pass
